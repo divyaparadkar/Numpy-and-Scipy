e21ae8da0eeb7a7f7235304f399b6e0393238fb4
Divyaparadkar@007 <114057036+divyaparadkar@users.noreply.github.com>
2023-05-10 21:59:14 +0530

    Add files via upload

---
+++ Arange.py
@@ -0,0 +1,5 @@
+import numpy as np
+help (np.arange)
+
+# arange(...)
+# arange([start,] stop[, step,], dtype=None, *, like=None)
---
+++ Arange_2.py
@@ -0,0 +1,8 @@
+import numpy as np
+a=np.arange(1,10)
+b=np.arange(3.0)
+c=np.arange(1,10,2)
+d=np.arange(20, dtype='complex')
+e= np.arange(1,10,2, dtype="float")
+
+print(a,b,c,d,e)
